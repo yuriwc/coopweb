f19c636a09050f4a3793bae4f10b802b1b4fd6e2
Yuri Matos <58712896+yuriwc@users.noreply.github.com>
2026-01-29 23:01:41 -0300

    update

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+import { ProxyConfig, NextRequest, NextResponse } from "next/server";
 
 const publicRoutes = [
   {
@@ -16,7 +16,7 @@ const publicRoutes = [
 const REDIRECT_WHEN_NOT_AUTHENTICATED = "/signin";
 const REDIRECT_WHEN_AUTHENTICATED = "/home";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const publicRoute = publicRoutes.find((route) =>
     route.isPrefix ? path.startsWith(route.path) : route.path === path
@@ -42,7 +42,7 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-export const config: MiddlewareConfig = {
+export const config: ProxyConfig = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
